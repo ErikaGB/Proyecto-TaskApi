4dcf9278116490aa05735fd6d4ffa3fa0a88cdba
Erika <gomeliiz@gmail.com>
2025-08-13 21:39:44 -0500

    taller todo

---
+++ DevWorkshop.TaskAPI.Application/DTOs/Auth/UserInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevWorkshop.TaskAPI.Application.DTOs.Auth
+{
+    public class UserInfo
+    {
+        /// <summary>
+        /// ID del usuario
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Nombre completo del usuario
+        /// </summary>
+        public string FullName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Email del usuario
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+    }
+
+}
